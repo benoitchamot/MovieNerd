efe0d84b4e2448971b951f930a0895a8bbba92d6
Benoit Chamot <benoit.chamot@gmail.com>
2023-09-12 16:27:37 +1000

    Add characters and actors data to API and Flask server. Update logic JS to import all data available from the API and save it in global variables.

--- Server/flask_server.py
+++ Server/flask_server.py
@@ -22,8 +22,11 @@ from flask_cors import CORS
 def str_to_date(in_string):
 # Convert a date stored as a string from the database and convert it to a datetime object.
 # Input string format: YYYY-MM-DD
-    out_date = dt.datetime.strptime(in_string, '%Y-%m-%d')
-    return dt.date(out_date.year, out_date.month, out_date.day)
+	out_date = dt.datetime.strptime(in_string, '%Y-%m-%d')
+	try:
+		return dt.date(out_date.year, out_date.month, out_date.day)
+	except:
+		return 0
 
 #########################################################
 # Database Setup
@@ -52,6 +55,8 @@ except Exception as inst:
 
 # Save references to each table
 movies = Base.classes.movies
+actors = Base.classes.actor
+characters = Base.classes.character
 
 #########################################################
 # Flask Setup
@@ -70,9 +75,17 @@ def home():
 	return (
 		f"<h1>Movies Nerd API</h1>"
 		f"<h2>Static routes</h2>"
-		f"<p><a href='http://127.0.0.1:5000/api/v1.0/movies'>/api/v1.0/movies</a></p>"
+		f"<p><a href='api/v1.0/movies'>/api/v1.0/movies</a></p>"
 		f"<ul>"
-		f"	<li>Returns data for all activities</li>"
+		f"	<li>Returns data for all movies</li>"
+		f"</ul>"
+		f"<p><a href='api/v1.0/actors'>/api/v1.0/actors</a></p>"
+		f"<ul>"
+		f"	<li>Returns data for all actors</li>"
+		f"</ul>"
+		f"<p><a href='api/v1.0/characters'>/api/v1.0/characters</a></p>"
+		f"<ul>"
+		f"	<li>Returns data for all characters</li>"
 		f"</ul>"
 		# f"<h2>Dynamic routes</h2>"
 		# f"<p><a href='http://127.0.0.1:5000/api/v1.0/u/1503960366'>/api/v1.0/u/&#x003C;user_name&#x003E;</a></p>"
@@ -82,8 +95,7 @@ def home():
 		# f"</ul>"
 	)
 
-# Static Activity Calories route
-# http://127.0.0.1:5000/api/v1.0/activities
+# Static Movies route
 @app.route("/api/v1.0/movies")
 def api_activities():
 	# Open session to the database
@@ -96,12 +108,12 @@ def api_activities():
 	# Loop through the measurements
 	for row in all_movies:    
     	# Add the data to a dictionary
-		act_dict = {'Id': row.movieid,
+		mov_dict = {'Id': row.movieid,
 			  'Title': row.title,
 			  'MPAA Rating': row.mpaa_rating,
 			  'Budget': row.budget,
 			  'Revenue': row.gross,
-			  'Release Date': str_to_date(row.release_date),
+			  'Release Date': row.release_date,
 			  'Genre': row.genre,
 			  'Runtime': row.runtime,
 			  'Rating': row.rating,
@@ -109,13 +121,73 @@ def api_activities():
 			  'Summary': row.summary}
 
 		# Append the data to the list of dictionary
-		movies_dicts.append(act_dict)
+		movies_dicts.append(mov_dict)
 
 	# Close session
 	session.close()
 
 	# Return jsonified dictionary
 	return jsonify(movies_dicts)
+
+# Static Actors route
+@app.route("/api/v1.0/actors")
+def api_actors():
+	# Open session to the database
+	session = Session(bind=engine)
+	all_actors = session.query(actors)
+	
+	# Create empty lists
+	actors_dicts = []
+
+	# Loop through the data points
+	for row in all_actors:    
+    	# Add the data to a dictionary
+		act_dict = {'Id': row.actorid,
+			  'Name': row.name,
+			  'DOB': row.date_of_birth,
+			  'City': row.birth_city,
+			  'Country': row.birth_country,
+			  'Height inches': row.height_inches,
+			  'Gender': row.gender,
+			  'Ethnicity': row.ethnicity,
+			  'Net worth': row.networth
+			  }
+		
+		# Append the data to the list of dictionary
+		actors_dicts.append(act_dict)
+
+	# Close session
+	session.close()
+
+	# Return jsonified dictionary
+	return jsonify(actors_dicts)
+
+# Static Characters route
+@app.route("/api/v1.0/characters")
+def api_characters():
+	# Open session to the database
+	session = Session(bind=engine)
+	all_characters = session.query(characters)
+	
+	# Create empty lists
+	characters_dicts = []
+
+	# Loop through the data points
+	for row in all_characters:    
+    	# Add the data to a dictionary
+		char_dict = {'Movie': row.movieid,
+			  'Actor': row.actorid,
+			  'Name': row.character_name
+			  }
+		
+		# Append the data to the list of dictionary
+		characters_dicts.append(char_dict)
+
+	# Close session
+	session.close()
+
+	# Return jsonified dictionary
+	return jsonify(characters_dicts)
 
 #########################################################
 # Run App
--- docs/static/js/logic.js
+++ docs/static/js/logic.js
@@ -1,5 +1,11 @@
 // API base URL
-let api_url = 'https://spiderdwarf.pythonanywhere.com/api/v1.0/'
+// let api_url = 'http://127.0.0.1:5000/api/v1.0/';
+let api_url = 'https://spiderdwarf.pythonanywhere.com/api/v1.0/';
+
+// Global variables
+let MOVIES = [];
+let ACTORS = [];
+let CHARACTERS = [];
 
 // Create table of movies
 function movieTable(movies){
@@ -9,21 +15,44 @@ function movieTable(movies){
         let movie = movies[i];
 
         // Append one table row `tr` to the table body
-        let new_tr = d3.select("#tableMovies>tbody").append("tr")
+        let new_tr = d3.select("#tableMovies>tbody").append("tr");
 
         // Append one cell for the movie title
-        new_tr.append("td").text(movie.Title)
-        new_tr.append("td").text(movie.Genre)
+        new_tr.append("td").text(movie.Title);
+        new_tr.append("td").text(movie.Genre);
     }
 }
 
 // Display movies
 function getMovies(data){
-    console.log(data);
+    MOVIES = data;
+    console.log("Movies data imported.");
+    console.log(MOVIES);
+    movieTable(MOVIES);
+}
 
-    movieTable(data);
+// Display actors
+function getActors(data){
+    ACTORS = data;
+    console.log("Actors data imported.");
+    console.log(ACTORS);
+}
+
+// Display characters
+function getCharacters(data){
+    CHARACTERS = data;
+    console.log("Characters data imported.");
+    console.log(CHARACTERS);
 }
 
 // Call API to get all movies
 let all_movies_url = api_url + 'movies';
 d3.json(all_movies_url).then(getMovies);
+
+// Call API to get all actors
+let all_actors_url = api_url + 'actors';
+d3.json(all_actors_url).then(getActors);
+
+// Call API to get all characters
+let all_characters_url = api_url + 'characters';
+d3.json(all_characters_url).then(getCharacters);
