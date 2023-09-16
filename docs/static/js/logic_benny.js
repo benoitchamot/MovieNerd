ee3ff7d6c6b6369a024cebe8d32580b314d81b38
Benoit Chamot <benoit.chamot@gmail.com>
2023-09-16 13:43:22 +1000

    Make API call from web-deployed version of API in gender wealth gap page

--- docs/static/js/logic_benny.js
+++ docs/static/js/logic_benny.js
@@ -1,7 +1,7 @@
 // API base URL
 // Run Flask server first: python flash_server.py
-// let api_url = 'https://spiderdwarf.pythonanywhere.com/api/v1.0/';
-let api_url = 'http://127.0.0.1:5000/api/v1.0/'
+let api_url = 'https://spiderdwarf.pythonanywhere.com/api/v1.0/';
+//let api_url = 'http://127.0.0.1:5000/api/v1.0/'
 
 // Global variable
 let top_N = 5;
